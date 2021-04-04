db611d15b0251f2307da060e26ba75aa974bf65a
vccheng2001 <vccheng@andrew.cmu.edu>
2021-04-04 15:50:49 -0700

    convert to numpy implementation

--- gnb.py
+++ gnb.py
@@ -2,59 +2,42 @@
 import pandas as pd 
 import numpy as np
 import math
+from collections import Counter
 '''
 Given an fMRI image of 21764 voxels, predict the associated stimulus word/class
 '''
-def main():
-    print(gaussian(1.0,1.0,1.0))
-    print(gaussian(2.0,1.0,1.0))
-    print(gaussian(0.0,1.0,1.0))
- 
-def hi():
-    data = pd.read_csv("tiny_data.csv", index_col = False)
-    # training examples 
-    # print(data.head())
-    # first_col = data.iloc[:, 0]
-    # print(first_col)
-    num_rows     = data.shape[0]
-    num_features = data.shape[1] - 1 # last column is label 
-    # get list of unique classes 
-    classes = data['label'].unique() 
-    # Step 1: calc independent class probabilities
-    priors = class_prior_probabilities(data, classes, num_rows)
-    # Step 2: calc class-conditional probability for each feature-class pair 
-    # class_cond_probs = class_cond_probabilities(data, classes, num_rows, num_features)
-    # Step 3: calc likelihood P(X|Y) for input image X 
-    likelihoods = calc_likelihoods(data, num_features)
-    # choose class with max probability 
 
-''' Calculates P(Y_i) for each class Y_i'''
-def class_prior_probabilities(data, classes, num_rows):
-    priors = {}
-    for yi in classes:
-        # number of rows where label == c
-        count = len(data.loc[data['label'] == yi])
-        # store prior probability for class c 
-        priors[yi] = count / num_rows
-    return priors
+def main():
+    data = np.genfromtxt("tiny_data.csv", delimiter=",", skip_header=1, dtype='unicode')
+    # tiny_data uses 200 features, 200 rows only
+    num_rows = data.shape[0]
+    num_features = data.shape[1] - 1 
+    # separate features, labels
+    features = data[:,:-1]
+    labels =   data[:,-1]
+
+    # Step 1: calc class priors P(Yi) for each class
+    classes = Counter(labels)   # dict of class counts 
+    priors = {k: (v/num_rows) for (k, v) in classes.items()}
+
+    # Step 2: calc P(Xi|Yi) for each feature-class pair
+    # class_cond_probs = class_cond_probs(data, classes, num_rows, num_features)
+    # Step 3: calc P(X|Y) for input image X 
+    likelihoods      = calc_likelihoods(data, num_features)
+    # choose class with max probability to make prediction
+
 
 ''' Calculates P(xi | yi) for each class-feature pair '''
-def class_cond_probabilities(data, classes, num_rows, num_features):
+def class_cond_probs(data, classes, num_rows, num_features):
     ccp = {}
     for yi in classes:
         for i in range(1, num_features + 1):  # data_1 through data_200
-             # value of ith feature
-
-            mean = 0
-            sigma = 0
-            # Gaussian distribution
-            left  = 1/math.sqrt(2*math.pi*sigma**2)
-            right = math.exp( (- (x_i - mean) **2) / (2 * (sigma**2)))
-            # store P(xi | yi)
-            ccp[yi][xi] = left * right 
+            xi = 0
+            # store P(xi|yi)
+            ccp[yi][xi] = gaussian(xi, mean, sigma)
     return ccp
 
-
+''' Gaussian pdf '''
 def gaussian(xi, mean, sigma):
     exp = math.exp( (- (xi - mean) **2) / (2 * (sigma**2)))
     return (1/math.sqrt(2*math.pi*sigma**2)) * exp
