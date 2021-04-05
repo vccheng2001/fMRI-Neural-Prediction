77d315c970ba94f4b223091d47fa7d481157d3f8
vccheng2001 <vccheng@andrew.cmu.edu>
2021-04-05 16:19:44 -0700

    Updated 4/5

--- gnb.py
+++ gnb.py
@@ -9,7 +9,19 @@ Gaussian Naive Bayes Classifier
 Given an fMRI image of 21764 voxels, predict the associated stimulus word/class
 '''
 
-def gaussian_naive_bayes(dataset):
+def main():
+    # Train 
+    train_dataset  = np.genfromtxt("data/train_medium.csv", delimiter= ",", \
+                                    skip_header=1, dtype='unicode')
+    (means, stdevs) = train_gnb(train_dataset)
+
+    # Test
+    test_dataset  = np.genfromtxt("data/test_medium.csv", delimiter= ",", \
+                                    skip_header=1, dtype='unicode')
+    test_gnb(test_dataset, means, stdevs)
+
+
+def train_gnb(dataset):
     # separate data, labels from training set 
     data   =   dataset[:,:-1]  
     labels =   dataset[:,-1]   # last column (ground truth y)
@@ -21,14 +33,13 @@ def gaussian_naive_bayes(dataset):
     # calc mean, stdev for each feature 
     means, stdevs = calc_mean_stdev(classes)
     # calc P(Yi|X) for input image X 
-    probs = calc_likelihoods(classes, data, priors, means, stdevs)
+    probs = calc_likelihoods(data, classes, priors, means, stdevs)
     # for each row, predict class yi
     preds = make_predictions(data, probs)
     err_rate = eval_error(preds, labels)
 
-    print(f"Finished training")
-    print(f"Accuracy: {1-err_rate}")
-
+    print(f"Finished training, Train accuracy: {1-err_rate}")
+    return (means, stdevs)
 
 ''' Group training data by class '''
 def split_data_by_class(dataset):
@@ -41,7 +52,7 @@ def split_data_by_class(dataset):
         classes[yi].append(X)
     return classes 
 
-''' Step 2: Calculates mean, stdev for each feature'''
+''' Calculates mean, stdev for each feature'''
 def calc_mean_stdev(classes):
     means = {}
     stdevs= {}
@@ -56,7 +67,7 @@ def gaussian_pdf(xi, mean, stdev):
     return (1/math.sqrt(2*math.pi*stdev**2)) * exp
 
 ''' Calculate likelihood that output belongs to a class yi given image X '''
-def calc_likelihoods(classes, data, priors, means, stdevs):
+def calc_likelihoods(data, classes, priors, means, stdevs):
     probs = {}
     num_features = data.shape[1]
 
@@ -73,10 +84,13 @@ def calc_likelihoods(classes, data, priors, means, stdevs):
             probs[i][yi] = prod 
     return probs
 
-''' For each row in training set, make class prediction '''
-def make_predictions(data, probs):
+''' For each row in dataset, make class prediction '''
+def make_predictions(data, probs, train=True):
     preds = []
-    out = open('predictions.txt', 'w')
+    if train:
+        out = open('predictions/predictions_train.txt', 'w')
+    else:
+        out = open('predictions/predictions_test.txt', 'w')
 
     for i in range(len(data)):
         # Select class that yields max likelihood
@@ -87,7 +101,7 @@ def make_predictions(data, probs):
     out.close()
     return preds
 
-''' Evaluate Training error '''
+''' Evaluate error '''
 def eval_error(preds, labels):
     errs = 0
     num_rows = len(labels)
@@ -99,8 +113,20 @@ def eval_error(preds, labels):
     err_rate =  errs/num_rows
     return err_rate
 
-if __name__ == "__main__":
-    dataset= np.genfromtxt("medium_data.csv", delimiter= ",", \
-                                    skip_header=1, dtype='unicode')
-    gaussian_naive_bayes(dataset)
+''' Test '''
+def test_gnb(test_dataset, means, stdevs):
+    # separate data, labels from training set 
+    test_data   =   test_dataset[:,:-1]  
+    test_labels =   test_dataset[:,-1]   # last column (ground truth y)
+    test_classes = split_data_by_class(test_dataset)
+    test_counts = Counter(test_labels) 
+    test_priors = {k: (v/len(test_data)) for k, v in test_counts.items()}
+    # group training data by class 
+    test_probs = calc_likelihoods(test_data, test_classes, test_priors, means, stdevs)
+    test_preds = make_predictions(test_data, test_probs, train=False)
+    test_err_rate = eval_error(test_preds, test_labels)
+    print(f"Finished testing, Test accuracy: {1-test_err_rate}")
 
+
+if __name__ == "__main__":
+    main()
