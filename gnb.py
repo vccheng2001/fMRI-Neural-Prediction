9a87f4b95cfae330fd8c34fa8c1f8648ee3534c8
vccheng2001 <vccheng@andrew.cmu.edu>
2021-04-07 19:52:48 -0700

    Select top k voxels, 100% test accuracy for 2 categories

--- gnb.py
+++ gnb.py
@@ -3,28 +3,25 @@ import pandas as pd
 import numpy as np
 import math
 from collections import Counter
-
 '''
 Gaussian Naive Bayes Classifier 
 Given an fMRI image of 21764 voxels, predict the associated stimulus word/class
 '''
 
-name = "full
+name = "data_tb"
 def main():
-    # Train 
+    k = 2000 # number of voxels to select 
+    # load train data 
     train_dataset  = np.genfromtxt(f"data/train_{name}.csv", delimiter= ",", \
-                                    skip_header=1, dtype='unicode')
-    (means, stdevs) = train_gnb(train_dataset)
-
+                                             skip_header=1, dtype='unicode')
+    (means, stdevs, priors, best_k_voxels) = train_gnb(train_dataset, k)
     # Test
-
-    print('\n')
     test_dataset  = np.genfromtxt(f"data/test_{name}.csv", delimiter= ",", \
                                     skip_header=1, dtype='unicode')
-    test_gnb(test_dataset, means, stdevs)
-
-
-def train_gnb(dataset):
+    test_gnb(test_dataset, means, stdevs, priors, best_k_voxels)
+   
+        
+def train_gnb(dataset, k):
     # separate data, labels from training set 
     data   =   dataset[:,:-1]  
     labels =   dataset[:,-1]   # last column (ground truth y)
@@ -32,20 +29,18 @@ def train_gnb(dataset):
     classes = split_data_by_class(dataset)
     # calc class priors P(Yi) for each class
     counts = Counter(labels) 
-    print(f"Train data shape: ", data.shape)
-    print(f"Train counts:", counts)
     priors = {k: (v/len(data)) for k, v in counts.items()}
-    print(f"Train Priors: {priors}")
     # calc mean, stdev for each feature 
-    means, stdevs = calc_mean_stdev(classes)
+    num_features = data.shape[1]
+    means, stdevs = calc_mean_stdev(classes, num_features)
+    best_k_voxels = select_best_voxels(means, stdevs, k)
     # calc P(Yi|X) for input image X 
-    probs = calc_likelihoods(data, classes, priors, means, stdevs)
+    probs = calc_likelihoods(data, classes, priors, means, stdevs, best_k_voxels)
     # for each row, predict class yi
     preds = make_predictions(data, probs)
-    err_rate = eval_error(preds, labels)
-
-    print(f"Finished training, Train accuracy: {1-err_rate}")
-    return (means, stdevs)
+    train_acc = eval_accuracy(preds, labels)
+    print(f"Finished training, Train accuracy: {train_acc}")
+    return (means, stdevs, priors, best_k_voxels)
 
 ''' Group training data by class '''
 def split_data_by_class(dataset):
@@ -58,13 +53,16 @@ def split_data_by_class(dataset):
         classes[yi].append(X)
     return classes 
 
+
 ''' Calculates mean, stdev for each feature'''
-def calc_mean_stdev(classes):
+def calc_mean_stdev(classes, num_features):
     means = {}
     stdevs= {}
+    num_classes = len(classes)
+    sum_stdevs = np.zeros(num_features)
     for yi, yi_rows in classes.items():
         means[yi] = np.mean(yi_rows, axis = 0)
-        stdevs[yi]= np.std(yi_rows, axis = 0)
+        stdevs[yi]= np.std(yi_rows, axis = 0) 
     return means, stdevs
 
 ''' Gaussian probability distribution function '''
@@ -73,18 +71,26 @@ def gaussian_pdf(xi, mean, stdev):
     pdf = (1/(math.sqrt(2*math.pi*stdev**2))) * exp
     return pdf 
 
+''' Select top k features to use  '''
+def select_best_voxels(means, stdevs, k):
+    diff = abs(means["tool"] - means["building"])
+    best_k_voxels = np.argpartition(diff, -k)[-k:]
+    return best_k_voxels
+
 ''' Calculate likelihood that output belongs to a class yi given image X '''
-def calc_likelihoods(data, classes, priors, means, stdevs):
+def calc_likelihoods(data, classes, priors, means, stdevs, best_k_voxels):
     probs = {}
     num_rows, num_features = data.shape
     # for each row in training data
     for i in range(len(data)):
+        # print(f"Row {i} of training set")
         X = data[i]         # Row i of training set 
         # for each class
         for yi in classes: 
             logsum = np.log(priors[yi])  # P(yi)
             if not probs.get(i): probs[i] = {}
-            for j in range(num_features):
+            # Only use the best k features 
+            for j in best_k_voxels:  
                 # multiply by P(xi | yi) for each feature 
                 gpdf = gaussian_pdf(float(X[j]), means[yi][j], stdevs[yi][j])
                 logsum +=  np.log(gpdf)
@@ -97,14 +103,11 @@ def make_predictions(data, probs, train=True):
     import itertools
     d = {k: probs[k] for k in list(probs.keys())[:5]}
 
-    print(d)
-
-
     preds = []
     if train:
-        out = open(f'predictions/train_{name}.txt', 'w')
+        out = open(f'predictions/train_{name}2000.txt', 'w')
     else:
-        out = open(f'predictions/test_{name}.txt', 'w')
+        out = open(f'predictions/test_{name}2000.txt', 'w')
 
     for i in range(len(data)):
         # Select class that yields max likelihood
@@ -116,7 +119,7 @@ def make_predictions(data, probs, train=True):
     return preds
 
 ''' Evaluate error '''
-def eval_error(preds, labels):
+def eval_accuracy(preds, labels):
     errs = 0
     num_rows = len(labels)
 
@@ -125,28 +128,22 @@ def eval_error(preds, labels):
             errs += 1
 
     err_rate =  errs/num_rows
-    return err_rate
+    return 1-err_rate
 
 ''' Test '''
-def test_gnb(test_dataset, means, stdevs):
-    # separate data, labels from training set 
+def test_gnb(test_dataset, means, stdevs, priors, best_k_voxels):
+    # Separate data, labels from training set 
     test_data   =   test_dataset[:,:-1]  
     test_labels =   test_dataset[:,-1]   # last column (ground truth y)
     test_classes = split_data_by_class(test_dataset)
-    test_counts = Counter(test_labels) 
-    print(f"Test data shape: ", test_data.shape)
-
-    print(f"Test counts:", test_counts)
-
-    test_priors = {k: (v/len(test_data)) for k, v in test_counts.items()}
-    print(f"Test Priors: {test_priors}")
-
-    # group training data by class 
-    test_probs = calc_likelihoods(test_data, test_classes, test_priors, means, stdevs)
+    # Calculate likelihoods given input test images 
+    test_probs = calc_likelihoods(test_data, test_classes, priors, means, stdevs, best_k_voxels)
+    # Make test predictions
     test_preds = make_predictions(test_data, test_probs, train=False)
-    test_err_rate = eval_error(test_preds, test_labels)
-    print(f"Finished testing, Test accuracy: {1-test_err_rate}")
-
+    # Test accuracy 
+    test_acc = eval_accuracy(test_preds, test_labels)
+    print(f"Finished testing, Test accuracy: {test_acc}")
+    return test_acc
 
 if __name__ == "__main__":
     main()
