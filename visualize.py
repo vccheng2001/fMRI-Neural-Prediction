3f17f054483ceaf143a9d61e67cfe6f3933b6611
vccheng2001 <vccheng@andrew.cmu.edu>
2021-04-16 17:34:46 -0700

    User can specify dataset in cmdline arg

--- visualize.py
+++ visualize.py
@@ -1,32 +1,34 @@
 import pandas as pd
 import numpy as np
 import csv 
+import sys 
 import matplotlib.pyplot as plt
 import imagesc as imagesc
 import scipy.io as c
 import pylab as pl                    
 
-
+# Visualize 2D fMRI brain slices using images from training data 
+# python3 visualize.py <path/to/data> <row_index> 
 def main():
-    voxels = 21764
+    num_voxels = 21764
     nx, ny, nz = 51, 61, 23
 
     # maps voxel index -> xyz coordinates
     col_to_coord = np.genfromtxt("col_to_coord.csv", delimiter=",")
     # stores all voxel values
-    train_data = np.genfromtxt("data/train_data.csv", delimiter=",", skip_header=1,  dtype='unicode')
+    data = np.genfromtxt(dataset, delimiter=",", skip_header=1,  dtype='unicode')
 
     # store xyz coords of each voxel 
     coords = {}
-    for i in range(voxels):
+    for i in range(num_voxels):
         coords[i] = col_to_coord[i]
 
-    label = train_data[0,-1]            # class category
-    voxel_values = train_data[0,:-1]    # feature values
+    label = data[row_index,-1]            # class category
+    voxel_values = data[row_index,:-1]    # feature values
 
     # construct 3d representation of image 
     img_3d = np.empty((nx,ny,nz))
-    for i in range(voxels):
+    for i in range(num_voxels):
         x = int(coords[i][0])
         y = int(coords[i][1])
         z = int(coords[i][2])
@@ -46,4 +48,7 @@ def main():
 
 
 if __name__ == "__main__":
+    # takes in row number from training data
+    (program, dataset, row_index) = sys.argv 
+    row_index = int(row_index)
     main()
